c2bcce4ffb3b4bdff64d0c38b6b2c571946968a4
Ben216k <91443760+Ben216k@users.noreply.github.com>
2024-08-03 18:12:24 -0600

    Initial Commit

---
+++ SwanTests/SwanTests.swift
@@ -0,0 +1,18 @@
+// 
+//  SwanTests.swift - SwanTests
+// 
+//  Created by Ben216k on 8/3/24
+//  Copyright (c) Ben216k (under 216k License)
+// 
+
+
+import Testing
+@testable import Swan
+
+struct SwanTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ SwanUITests/SwanUITestsLaunchTests.swift
@@ -0,0 +1,34 @@
+// 
+//  SwanUITestsLaunchTests.swift - SwanUITests
+// 
+//  Created by Ben216k on 8/3/24
+//  Copyright (c) Ben216k (under 216k License)
+// 
+
+
+import XCTest
+
+final class SwanUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
